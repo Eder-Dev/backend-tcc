9976c8a5c31b1d18ed1ed40dea6b7c6b80e057de
Eder-Dev <60243316+Eder-Dev@users.noreply.github.com>
2022-12-13 11:17:49 -0300

    test

--- indes.js
+++ indes.js
@@ -10,5 +10,5 @@ app.use(express.json())
 app.use(routes)
 
 //Start server
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server started on port localhost:${PORT}`));
